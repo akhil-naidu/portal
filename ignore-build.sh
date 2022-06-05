6bb0bf5f1a524f13b46babc53c75271e405449a7
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-06-05 21:00:55 +0530

    new: ignore-build.sh script

---
+++ ignore-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+var=$(git diff HEAD^ HEAD --quiet ./hasura)
+
+
+if [[ var ]] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
