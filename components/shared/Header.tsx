f67e121c0b015d560e0d70d289cfc3bc88dbe1b8
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-06-15 19:57:25 +0530

    feat: responsive header with avatar menu

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,13 +1,25 @@
-import { Messages, Notification, Search, Settings } from 'tabler-icons-react';
+import {
+  ArrowsLeftRight,
+  MessageCircle,
+  Messages,
+  Notification,
+  Photo,
+  Search,
+  Settings,
+} from 'tabler-icons-react';
 
 import {
   ActionIcon,
   Avatar,
+  Box,
   Burger,
+  Divider,
   Group,
   Input,
   MediaQuery,
+  Menu,
   Text,
+  UnstyledButton,
 } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
 
@@ -56,12 +68,59 @@ const Header = ({ theme }: any) => {
             </ActionIcon>
           </Group>
         )}
-        <Avatar
-          src='https://api.lorem.space/image/face?hash=33791'
-          alt='Profile Picture of the User'
-          radius='xl'
-          size='sm'
-        />
+
+        {/* Avatar Menu */}
+        <Menu
+          trigger='click'
+          control={
+            <UnstyledButton
+              sx={(theme) => ({
+                display: 'block',
+                width: '100%',
+                padding: theme.spacing.xs,
+                borderRadius: theme.radius.sm,
+                color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+
+                '&:hover': {
+                  backgroundColor:
+                    theme.colorScheme === 'dark'
+                      ? theme.colors.dark[6]
+                      : theme.colors.gray[0],
+                },
+              })}
+            >
+              <Avatar
+                src='https://api.lorem.space/image/face?hash=33791'
+                alt='Profile Picture of the User'
+                radius='xl'
+                size='sm'
+              />
+            </UnstyledButton>
+          }
+        >
+          <Box>
+            <Menu.Label>Application</Menu.Label>
+            <Menu.Item icon={<Settings size={14} />}>Dashboard</Menu.Item>
+            <Menu.Item icon={<MessageCircle size={14} />}>Create Post</Menu.Item>
+            <Menu.Item icon={<Photo size={14} />}>Reading List</Menu.Item>
+
+            <Divider />
+          </Box>
+          {!showHeaderActions && (
+            <Box>
+              <Menu.Label>Hidden</Menu.Label>
+              <Menu.Item icon={<Settings size={14} />}>Settings</Menu.Item>
+              <Menu.Item icon={<MessageCircle size={14} />}>Messages</Menu.Item>
+              <Menu.Item icon={<Photo size={14} />}>Gallery</Menu.Item>
+
+              <Divider />
+            </Box>
+          )}
+
+          <Box>
+            <Menu.Item icon={<ArrowsLeftRight size={14} />}>Logout</Menu.Item>
+          </Box>
+        </Menu>
       </Group>
     </Group>
   );
