9a80546117cf5562877232a5bd0f501a3d714d81
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-06-17 01:03:38 +0530

    chore: default loader as dots

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,6 +30,7 @@ export default function App(props: AppProps) {
           theme={{
             /** Put your mantine theme override here */
             colorScheme: currentTheme,
+            loader: 'dots',
           }}
         >
           <Component {...pageProps} />
