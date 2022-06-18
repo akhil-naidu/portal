eff27f6858fee86753ef2a0597882d2c6c7136b4
chalumurigirish <104982249+chalumurigirish@users.noreply.github.com>
2022-06-18 17:32:27 +0530

    feat: sidebar (#9)

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,12 +1,108 @@
-import React from 'react';
+import {
+  Aside,
+  Avatar,
+  Box,
+  Group,
+  MediaQuery,
+  Paper,
+  ScrollArea,
+  Stack,
+  Text,
+  UnstyledButton,
+} from '@mantine/core';
 
-import { Aside, MediaQuery, Text } from '@mantine/core';
+import customButtonStyles from './customButtonStyles';
+
+const SidebarData = [
+  {
+    title: 'Trending posts',
+    data: [
+      {
+        image:
+          'https://www.bhaktiphotos.com/wp-content/uploads/2018/04/Mahadev-Bhagwan-Photo-for-Devotee.jpg',
+        text: 'Paper is the most basic ui component',
+      },
+      {
+        image:
+          'https://www.bhaktiphotos.com/wp-content/uploads/2018/04/Mahadev-Bhagwan-Photo-for-Devotee.jpg',
+        text: 'Paper is the most basic ui component',
+      },
+      {
+        image:
+          'https://www.bhaktiphotos.com/wp-content/uploads/2018/04/Mahadev-Bhagwan-Photo-for-Devotee.jpg',
+        text: 'Paper is the most basic ui component',
+      },
+      {
+        image:
+          'https://www.bhaktiphotos.com/wp-content/uploads/2018/04/Mahadev-Bhagwan-Photo-for-Devotee.jpg',
+        text: 'Paper is the most basic ui component',
+      },
+      {
+        image:
+          'https://www.bhaktiphotos.com/wp-content/uploads/2018/04/Mahadev-Bhagwan-Photo-for-Devotee.jpg',
+        text: 'Paper is the most basic ui component',
+      },
+      {
+        image:
+          'https://www.bhaktiphotos.com/wp-content/uploads/2018/04/Mahadev-Bhagwan-Photo-for-Devotee.jpg',
+        text: 'Paper is the most basic ui component',
+      },
+      {
+        image:
+          'https://www.bhaktiphotos.com/wp-content/uploads/2018/04/Mahadev-Bhagwan-Photo-for-Devotee.jpg',
+        text: 'Paper is the most basic ui component',
+      },
+      {
+        image:
+          'https://www.bhaktiphotos.com/wp-content/uploads/2018/04/Mahadev-Bhagwan-Photo-for-Devotee.jpg',
+        text: 'Paper is the most basic ui component',
+      },
+    ],
+  },
+];
 
 const Sidebar = () => {
   return (
     <MediaQuery smallerThan='md' styles={{ display: 'none' }}>
-      <Aside p='md' hiddenBreakpoint='md' width={{ sm: 200, lg: 300 }}>
-        <Text>Application sidebar</Text>
+      <Aside hiddenBreakpoint='md' width={{ sm: 200, lg: 300 }}>
+        <Stack>
+          <Box>
+            <Text size='md' weight={500} color={'dimmed'} pt='md' px='xs'>
+              {SidebarData[0].title}
+            </Text>
+            <Paper
+              component={ScrollArea}
+              m='xs'
+              p='xs'
+              radius='md'
+              shadow='xs'
+              sx={{ height: 250 }}
+              withBorder
+            >
+              {SidebarData[0].data.map((data, index) => {
+                return (
+                  <UnstyledButton
+                    key={index}
+                    sx={(theme) =>
+                      customButtonStyles(theme, {
+                        padding: '0px',
+                        paddingTop: '8px',
+                        paddingBottom: '8px',
+                        borderRadius: '8px',
+                      })
+                    }
+                  >
+                    <Group noWrap>
+                      <Avatar alt={data.text} radius='xl' src={data.image} />
+
+                      <Text size='sm'>{data.text}</Text>
+                    </Group>
+                  </UnstyledButton>
+                );
+              })}
+            </Paper>
+          </Box>
+        </Stack>
       </Aside>
     </MediaQuery>
   );
